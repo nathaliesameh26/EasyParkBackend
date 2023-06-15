f1de91721473f088ed8fa40fcb497263354f04c4
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-06-16 00:33:10 +0300

    remove main from voice

--- Flask Code/api-voice.py
+++ Flask Code/api-voice.py
@@ -144,5 +144,5 @@ def preprocess_data(data):
     return preprocessed_data
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000,host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8000,host='0.0.0.0')
