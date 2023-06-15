ff8dc2cf73d5430894bcbfda1b69dc52b1328739
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-06-16 00:33:39 +0300

    remove main from try

--- Flask Code/try.py
+++ Flask Code/try.py
@@ -92,5 +92,5 @@ def upload():
         # "message2": ypred_parkinson
         })
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8000, host='0.0.0.0')
