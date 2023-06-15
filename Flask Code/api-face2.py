595e4b433778846c7d8972f215b456b4524b7291
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-06-16 00:33:02 +0300

    remove main from face2

--- Flask Code/api-face2.py
+++ Flask Code/api-face2.py
@@ -93,8 +93,8 @@ def upload():
     # Return the results as JSON
     return jsonify({"message": results})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8000, host='0.0.0.0')
 
 
 
